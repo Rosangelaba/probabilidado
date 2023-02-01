f7b85474639c184b454718b383dea80a67eb84fe
Yuri Kaszubowski Lopes <yurikazuba@gmail.com>
2023-02-01 20:57:25 -0300

    update models

--- server/probdado/admin.py
+++ server/probdado/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from.import models
 # Register your models here.
+admin.site.register(models.Combinacao)
+admin.site.register(models.Dica)
--- server/probdado/models.py
+++ server/probdado/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Combinacao(models.Model):
+    dado1 = models.IntegerField()
+    dado2 = models.IntegerField()
+#    CorDado1 = models.CharField(max_length=10)
+#    CorDado2 = models.CharField(max_length=10)
+
+class Dica(models.Model):
+    text = models.CharField(max_length=256)
+    combinacoes = models.ManyToManyField(Combinacao)
+
